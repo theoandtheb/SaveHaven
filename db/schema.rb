4a52ac0cafd7863556385a58e16e01d09b07d3c7
Justine Jones <justinejones91@gmail.com>
2014-09-14 06:19:06 -0400

    ran a migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140913194909) do
+ActiveRecord::Schema.define(version: 20140914033455) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,12 @@ ActiveRecord::Schema.define(version: 20140913194909) do
     t.string   "image"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "slug"
+    t.string   "avatar"
+    t.string   "phone"
   end
+
+  add_index "tags", ["slug"], name: "index_tags_on_slug", unique: true, using: :btree
 
   create_table "users", force: true do |t|
     t.string   "username"
